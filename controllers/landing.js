d15ff2ce63f7238566b05f03de99be49185299dc
sserzant <ssergeje1@gmail.com>
2020-06-16 04:30:31 +0800

    added lead editing

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,8 +1,57 @@
+const models = require('../models');
+
 exports.get_landing = function(req, res, next) {
     res.render('landing', { title: 'Express' });
 }
 
 exports.submit_lead = function(req, res, next) {
     console.log("lead email: ", req.body.lead_email);
-    res.redirect('/');
+    return models.Lead.create({
+        email: req.body.lead_email
+    }).then(lead => {
+        res.redirect('/leads');
+    })
+
+}
+
+exports.show_leads = function(req, res, next) {
+    return models.Lead.findAll().then(leads => {
+        res.render('landing', { title: 'Express', leads: leads });
+    })
+
+}
+
+exports.show_lead = function(req, res, next) {
+    return models.Lead.findOne({
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(lead => {
+        res.render('lead', { title: 'Lead', lead: lead });
+    });
+
+}
+
+exports.show_edit_lead = function(req, res, next) {
+    return models.Lead.findOne({
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(lead => {
+        res.render('lead/edit_lead', { title: 'Lead', lead: lead });
+    });
+
+}
+
+exports.edit_lead = function(req, res, next) {
+
+    return models.Lead.update({
+        email: req.body.lead_email
+    }, {
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(result => {
+        res.redirect('/lead/' + req.params.lead_id);
+    })
 }
